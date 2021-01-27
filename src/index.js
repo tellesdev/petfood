06b589deb55a77744b36f65ee9c95a69a0381be8
Alexsander Telles <git@alextelles.dev.br>
2021-01-27 15:08:57 -0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Cadastro from './pages/cadastro'
-import './styles/global.css'
+import Cadastro from './pages/cadastro';
+import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Cadastro />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,10 +1,18 @@
-const Cadastro = () =>{
+import Logo from '../../assets/logo.png';
+import Ilustration from '../../assets/illustration.png'
+
+const Cadastro = () => {
   return (
     <div className="container-fluid h-100 bg-primary">
-      
-
+      <div className="col-12">
+        <img src={Logo} />
+      </div>
+      <div className="col-6">
+        <img src={Ilustration}/>
+      </div>
+      <div className="col-6"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Cadastro;
